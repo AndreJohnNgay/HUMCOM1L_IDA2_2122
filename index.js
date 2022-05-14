c4db289693bf1e39755614e990a8f39c4c306129
AndreJohnNgay <98294114+AndreJohnNgay@users.noreply.github.com>
2022-05-14 23:46:13 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Hello World');
+
+let name = 'Baguio';
+console.log(name);
+
+let firstName = 'City';
+let lastName = 'Summer Capital of Ph';
+
+const interestRate = 0.4;
+console.log(interestRate);
