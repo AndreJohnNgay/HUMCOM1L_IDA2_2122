5ab1fa334ca816627b62722c39ee80b30a02d841
AndreJohnNgay <98294114+AndreJohnNgay@users.noreply.github.com>
2022-05-22 23:36:26 +0800

    Update Exercise 16.js

--- Exercise 16.js
+++ Exercise 16.js
@@ -1,2 +1,5 @@
-let html = document.getElementById("myP").innerHTML;
-document.getElementById("innerHTML").innerHTML = html;
+let first_name = "Andre John",
+    last_name = "Ngay",
+    age = "19";
+let days = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
+document.getElementById("three_var").innerHTML = first_name, last_name, age, days;
