b31d112cf384459b7385fcc8e21cfd09cef6999a
AndreJohnNgay <98294114+AndreJohnNgay@users.noreply.github.com>
2022-05-22 23:22:54 +0800

    Update Exercise 15.js

--- Exercise 15.js
+++ Exercise 15.js
@@ -1,2 +1,20 @@
-let html = document.getElementById("myP").innerHTML;
-document.getElementById("innerHTML").innerHTML = html;
+document.getElementById("firstName").value="Tyrael Martinez";
+
+document.getElementById("age").value="18";
+
+document.getElementById("phoneNumber").value="09187321321";
+
+document.getElementById("emailAddress").value="20191232@email.com";
+
+document.getElementById("mailingAddress").value="Assumption Road, Baguio City";
+
+document.getElementById("program").value="Computer Science";
+
+document.getElementById("idNumber").value="2019132";
+
+document.getElementById("yearLevel").value="2nd Year";
+
+document.getElementById("section").value="IAB";
+
+
+
